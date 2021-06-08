1af909cdf4128e2e4b374277b1e8e4179d689953
Arya Hirode <aryahirode2001@gmail.com>
2021-06-08 15:35:53 -0400

    wrote unit tests for PasswordStrength

---
+++ src/test/java/org/example/ex25/base/PasswordStrengthTest.java
@@ -0,0 +1,40 @@
+package org.example.ex25.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordStrengthTest {
+
+    @Test
+    void passwordValidator_returns_1_for_very_weak_passwords() {
+        PasswordStrength pw = new PasswordStrength();
+        int expected = 1;
+        int actual = pw.passwordValidator("1234567");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordValidator_returns_2_for_weak_passwords() {
+        PasswordStrength pw = new PasswordStrength();
+        int expected = 2;
+        int actual = pw.passwordValidator("abcdefg");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordValidator_returns_3_for_strong_passwords() {
+        PasswordStrength pw = new PasswordStrength();
+        int expected = 3;
+        int actual = pw.passwordValidator("abcdefg1");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordValidator_returns_4_for_very_strong_passwords() {
+        PasswordStrength pw = new PasswordStrength();
+        int expected = 4;
+        int actual = pw.passwordValidator("abcdefg1@");
+        assertEquals(expected, actual);
+    }
+}
