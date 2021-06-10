3f89dde741096945958fffc5213b5ef08423a7ca
Arya Hirode <aryahirode2001@gmail.com>
2021-06-10 18:24:40 -0400

    Completed tests

---
+++ src/test/java/org/example/ex29/base/InvestmentCalculatorTest.java
@@ -0,0 +1,28 @@
+package org.example.ex29.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvestmentCalculatorTest {
+
+    @Test
+    void quickCalculator_returns_correct_value1() {
+        InvestmentCalculator ic = new InvestmentCalculator();
+        double expected = 18;
+        double actual = ic.quickCalculator(4);
+        double delta = 1;
+        assertEquals(expected, actual, delta);
+    }
+
+    @Test
+    void quickCalculator_returns_correct_value2() {
+        InvestmentCalculator ic = new InvestmentCalculator();
+        double expected = Double.POSITIVE_INFINITY;
+        double actual = ic.quickCalculator(Double.MIN_VALUE);
+        double delta = 1;
+        assertEquals(expected, actual, delta);
+    }
+
+
+}
